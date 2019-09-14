6faf33f791867dc920dbf44483c37d36d5db5757
waryanto <waryanto89@gmail.com>
2019-09-14 23:56:53 +0700

    Update html-chords.js

--- html-chords.js
+++ html-chords.js
@@ -89,6 +89,6 @@ $(document).ready(function() {
 		case "ru":
 			gcwriter.init("Изменить тональность", "на полтона вверх", "на полтона вниз");
 		default:
-			gcwriter.init("Transpose", "Naik", "Turun");
+			gcwriter.init("<strong>Transpose</strong>", "<strong>+1</strong>", "<strong>-1</strong>");
 	}
 });
