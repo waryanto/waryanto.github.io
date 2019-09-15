d228d38e18551804b8d7bab2fd1c529e0fce4ff2
waryanto <waryanto89@gmail.com>
2019-09-15 23:11:56 +0700

    Update html-chords.js

--- html-chords.js
+++ html-chords.js
@@ -88,6 +88,6 @@ $(document).ready(function() {
 	switch (lang) {
 		
 		default:
-			gcwriter.init("<strong>Ganti Kunci</strong>", "\u2BC5", "\u2BC6");
+			gcwriter.init("<strong>Ganti Kunci</strong>", "&#11205;", "&#11206;");
 	}
 });
